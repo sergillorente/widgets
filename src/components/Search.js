35d0c0eb991591742ca4e38b7874bf9c48138f61
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-01-22 19:26:10 +0100

    Controlling the request I do to the API with conditional statements

--- src/components/Search.js
+++ src/components/Search.js
@@ -21,14 +21,19 @@ const Search = () => {
 
             setResults(data.query.search);
         };
-        const timeoutId = setTimeout(() =>{
-            if (term) {
-                search();
-            }
-        }, 500);
 
-        return () => {
-            clearTimeout(timeoutId);
+        if (term && !results.length) {
+            search();
+        } else {
+            const timeoutId = setTimeout(() => {
+                if (term) {
+                    search();
+                }
+            }, 1000);
+    
+            return () => {
+                clearTimeout(timeoutId);
+            };
         };
 
     }, [term])
