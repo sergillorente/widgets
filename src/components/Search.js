1a9e65c9cd3102625f04a1b46f04002f8be3d996
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-01-22 10:30:07 +0100

    Trying the useEffect in hooks

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Search = () => {
-    return <h1>Search</h1>;
+    const [term, setTerm] = useState('');
+
+    console.log('I run with every render');
+
+    useEffect(() => {
+        console.log('I run after every render and at initial render');
+    }, [term])
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input
+                        value={term}
+                        onChange={e => setTerm(e.target.value)}
+                        className="input"
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Search;
