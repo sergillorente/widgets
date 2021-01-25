fdf3c86e5e1be494849476e2b270e9c8f6bddc15
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-01-25 17:59:39 +0100

    Use for the first time the useRef to ref specific elements of the DOM and for that, determine what's the action we want to apply

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        document.body.addEventListener('click', (event) => {
+            if (ref.current && ref.current.contains(event.target)) {
+                return; 
+            };
+            setOpen(false);
+        }, {capture: true});
+    }, []);
 
     const renderedOptions = options.map((option) => {
         if (option.value === selected.value) {
@@ -19,7 +29,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     });
 
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">
                     Select a Color
