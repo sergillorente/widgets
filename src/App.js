245d5ca4a58a5d2c1124bd8a03d54b5731ce3c25
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-01-25 13:56:39 +0100

    Dropdown file created. Some JSX added and testing if the useState works properly

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
-const items =[
+const items = [
     {
         title: 'What is React?',
         content: 'React is a front end javascript framework',
@@ -15,11 +16,32 @@ const items =[
         title: 'How do you use React?',
         content: 'You use React by creating components'
     }
+];
+
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'blue'
+    }
 ]
 
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (<div>
-        <Search />
+        <Dropdown
+            selected={selected}
+            onSelectedChange={setSelected}
+            options={options}
+        />
     </div>
     );
 };
