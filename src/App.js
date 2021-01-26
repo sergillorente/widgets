5bffb310584923ef8e824d3c39b327cd426dd13d
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-01-26 18:32:20 +0100

    FE routes created with React Router and a component called Router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
 
 const items = [
     {
@@ -58,14 +59,34 @@ const showTranslate = () => {
     }
 };
 
+const showComponent = (route, component) => {
+    return window.location.pathname === route
+        ? component
+        : null
+}
+
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
 
     return (
         <div>
-            {showAccordion()}
-            {showList()}
-            {showDropdown()}
-            {showTranslate()}
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown 
+                    label="Select a Color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     );
 };
